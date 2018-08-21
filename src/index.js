799502d8b85db1ae68c46d59f202258818d58068
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-08-21 15:08:24 +0530

    feat: move index.js to src/index.js; compile down to ES5 using babel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.TransitionGroup = exports.Transition = exports.onEnd = exports.nextFrame = exports.update = exports.remove = exports.insert = exports.create = undefined;
-
-var _snabbdom = require('snabbdom');
+import { h } from 'snabbdom';
 
 /*
  * Source: https://vuejs.org/v2/guide/transitions.html#Transition-Classes
@@ -37,24 +30,24 @@ var _snabbdom = require('snabbdom');
  * the transition/animation finishes.
 */
 
-var addClass = function addClass(elm, classes) {
+const addClass = (elm, classes) => {
   classes = classes.split(/\s+/);
-  for (var i = 0; i < classes.length; i++) {
+  for (let i = 0; i < classes.length; i++) {
     elm.classList.add(classes[i]);
   }
 };
 
-var removeClass = function removeClass(elm, classes) {
+const removeClass = (elm, classes) => {
   classes = classes.split(/\s+/);
-  for (var i = 0; i < classes.length; i++) {
+  for (let i = 0; i < classes.length; i++) {
     elm.classList.remove(classes[i]);
   }
 };
 
-var create = exports.create = function create(oldVnode, vnode) {
-  var t = vnode.data.transition;
+export const create = (oldVnode, vnode) => {
+  const t = vnode.data.transition;
   if (t) {
-    var elm = vnode.elm;
+    const elm = vnode.elm;
     removeClass(elm, t.enter);
     removeClass(elm, t.enterActive);
     removeClass(elm, t.enterTo);
@@ -63,26 +56,26 @@ var create = exports.create = function create(oldVnode, vnode) {
   }
 };
 
-var insert = exports.insert = function insert(vnode) {
-  var t = vnode.data.transition;
+export const insert = vnode => {
+  const t = vnode.data.transition;
   if (t) {
-    var elm = vnode.elm;
+    const elm = vnode.elm;
     removeClass(elm, t.enterTo);
-    nextFrame(function () {
+    nextFrame(() => {
       removeClass(elm, t.enter);
       addClass(elm, t.enterTo);
     });
-    onEnd(elm, function () {
+    onEnd(elm, () => {
       removeClass(elm, t.enterTo);
       removeClass(elm, t.enterActive);
     });
   }
 };
 
-var remove = exports.remove = function remove(vnode, callback) {
-  var t = vnode.data.transition;
+export const remove = (vnode, callback) => {
+  const t = vnode.data.transition;
   if (t) {
-    var elm = vnode.elm;
+    const elm = vnode.elm;
     removeClass(elm, t.leave);
     removeClass(elm, t.leaveActive);
     removeClass(elm, t.leaveTo);
@@ -93,11 +86,11 @@ var remove = exports.remove = function remove(vnode, callback) {
     }
     addClass(elm, t.leave);
     addClass(elm, t.leaveActive);
-    nextFrame(function () {
+    nextFrame(() => {
       removeClass(elm, t.leave);
       addClass(elm, t.leaveTo);
     });
-    onEnd(elm, function () {
+    onEnd(elm, () => {
       removeClass(elm, t.leaveTo);
       removeClass(elm, t.leaveActive);
       callback();
@@ -105,23 +98,23 @@ var remove = exports.remove = function remove(vnode, callback) {
   }
 };
 
-var update = exports.update = function update(oldVnode, vnode) {
-  var t = vnode.data.transition;
+export const update = (oldVnode, vnode) => {
+  const t = vnode.data.transition;
   if (t) {
-    var oldRect = oldVnode.data.transitionGroupRect;
+    const oldRect = oldVnode.data.transitionGroupRect;
     if (oldRect) {
-      var elm = oldVnode.elm;
+      const elm = oldVnode.elm;
       removeClass(elm, t.move);
-      nextFrame(function () {
-        var newRect = elm.getBoundingClientRect();
-        var dx = oldRect.x - newRect.x;
-        var dy = oldRect.y - newRect.y;
+      nextFrame(() => {
+        const newRect = elm.getBoundingClientRect();
+        const dx = oldRect.x - newRect.x;
+        const dy = oldRect.y - newRect.y;
         if (dx || dy) {
-          elm.style.transform = 'translate(' + dx + 'px, ' + dy + 'px)';
-          nextFrame(function () {
+          elm.style.transform = `translate(${dx}px, ${dy}px)`;
+          nextFrame(() => {
             addClass(elm, t.move);
             elm.style.transform = '';
-            onEnd(elm, function () {
+            onEnd(elm, () => {
               removeClass(elm, t.move);
             });
           });
@@ -131,12 +124,12 @@ var update = exports.update = function update(oldVnode, vnode) {
   }
 };
 
-var nextFrame = exports.nextFrame = requestAnimationFrame || setTimeout;
+export const nextFrame = requestAnimationFrame || setTimeout;
 
-var onEnd = exports.onEnd = function onEnd(elm, callback) {
-  var remaining = countAnimationsAndTransitions(elm);
+export const onEnd = (elm, callback) => {
+  let remaining = countAnimationsAndTransitions(elm);
   if (remaining === 0) return callback();
-  var listener = function listener(event) {
+  const listener = event => {
     if (event.target === elm) remaining--;
     if (remaining === 0) {
       elm.removeEventListener('transitionend', listener);
@@ -149,16 +142,16 @@ var onEnd = exports.onEnd = function onEnd(elm, callback) {
 };
 
 // Source: https://github.com/vuejs/vue/blob/59860b0a756526f37468655598c68d119f0e74bd/src/platforms/web/runtime/transition-util.js
-var countAnimationsAndTransitions = function countAnimationsAndTransitions(elm) {
-  var styles = window.getComputedStyle(elm);
-  var transitionDelays = styles['transitionDelay'].split(', ');
-  var transitionDurations = styles['transitionDuration'].split(', ');
-  var transitionTimeout = getTimeout(transitionDelays, transitionDurations);
-  var animationDelays = styles['animationDelay'].split(', ');
-  var animationDurations = styles['animationDuration'].split(', ');
-  var animationTimeout = getTimeout(animationDelays, animationDurations);
+const countAnimationsAndTransitions = elm => {
+  const styles = window.getComputedStyle(elm);
+  const transitionDelays = styles['transitionDelay'].split(', ');
+  const transitionDurations = styles['transitionDuration'].split(', ');
+  const transitionTimeout = getTimeout(transitionDelays, transitionDurations);
+  const animationDelays = styles['animationDelay'].split(', ');
+  const animationDurations = styles['animationDuration'].split(', ');
+  const animationTimeout = getTimeout(animationDelays, animationDurations);
 
-  var count = 0;
+  let count = 0;
   if (transitionTimeout > 0) count += transitionDurations.length;
   if (animationTimeout > 0) count += animationDurations.length;
   return count;
@@ -168,9 +161,12 @@ var countAnimationsAndTransitions = function countAnimationsAndTransitions(elm) 
       delays = delays.concat(delays);
     }
 
-    return Math.max.apply(null, durations.map(function (d, i) {
-      return toMs(d) + toMs(delays[i]);
-    }));
+    return Math.max.apply(
+      null,
+      durations.map((d, i) => {
+        return toMs(d) + toMs(delays[i]);
+      })
+    );
   }
 
   function toMs(s) {
@@ -178,37 +174,41 @@ var countAnimationsAndTransitions = function countAnimationsAndTransitions(elm) 
   }
 };
 
-var normalize = function normalize(o) {
-  o = o ? typeof o === 'string' ? { prefix: o } : o : { prefix: 's' };
-  var prefix = o.prefix;
+const normalize = o => {
+  o = o ? (typeof o === 'string' ? { prefix: o } : o) : { prefix: 's' };
+  const prefix = o.prefix;
 
-  if (!o.enter) o.enter = prefix + '-enter';
-  if (!o.enterActive) o.enterActive = prefix + '-enter-active';
-  if (!o.enterTo) o.enterTo = prefix + '-enter-to';
-  if (!o.leave) o.leave = prefix + '-leave';
-  if (!o.leaveActive) o.leaveActive = prefix + '-leave-active';
-  if (!o.leaveTo) o.leaveTo = prefix + '-leave-to';
-  if (!o.move) o.move = prefix + '-move';
+  if (!o.enter) o.enter = `${prefix}-enter`;
+  if (!o.enterActive) o.enterActive = `${prefix}-enter-active`;
+  if (!o.enterTo) o.enterTo = `${prefix}-enter-to`;
+  if (!o.leave) o.leave = `${prefix}-leave`;
+  if (!o.leaveActive) o.leaveActive = `${prefix}-leave-active`;
+  if (!o.leaveTo) o.leaveTo = `${prefix}-leave-to`;
+  if (!o.move) o.move = `${prefix}-move`;
 
   return o;
 };
 
-var Transition = exports.Transition = function Transition(o, sel, data, children) {
+export const Transition = (o, sel, data, children) => {
   data.transition = normalize(o);
-  data.hook = { create: create, insert: insert, update: update, remove: remove };
-  return (0, _snabbdom.h)(sel, data, children);
+  data.hook = { create, insert, update, remove };
+  return h(sel, data, children);
 };
 
-var TransitionGroup = exports.TransitionGroup = function TransitionGroup(o, sel, data, children) {
-  var update = function update(oldVnode, vnode) {
-    for (var i = 0; i < oldVnode.children.length; i++) {
-      var child = oldVnode.children[i];
+export const TransitionGroup = (o, sel, data, children) => {
+  const update = (oldVnode, vnode) => {
+    for (let i = 0; i < oldVnode.children.length; i++) {
+      const child = oldVnode.children[i];
       child.data.transitionGroupRect = child.elm.getBoundingClientRect();
     }
   };
-  data.hook = { update: update };
-  return (0, _snabbdom.h)(sel, data, children.map(function (child) {
-    child.data.isInTransitionGroup = true;
-    return Transition(o, child.sel, child.data, child.children);
-  }));
+  data.hook = { update };
+  return h(
+    sel,
+    data,
+    children.map(child => {
+      child.data.isInTransitionGroup = true;
+      return Transition(o, child.sel, child.data, child.children);
+    })
+  );
 };
